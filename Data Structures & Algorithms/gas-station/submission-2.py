2905f5961fd37d12e2925fc001a6d21a465a65f0
Anver Ismagilov <64646822+Lanv3r@users.noreply.github.com>
2026-07-30 12:13:26 -0400

    Add: gas-station - submission-2

---
+++ Data Structures & Algorithms/gas-station/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        n = len(gas)
+        total_gas = sum(gas)
+        total_cost = sum(cost)
+        if total_gas < total_cost:
+            return -1
+            
+        def greedy(i):
+            tank = 0
+            for k in range(n):
+                j = (k + i) % n
+                tank += gas[j] - cost[j]
+                if tank < 0:
+                    new_ind = (j + 1) % n
+                    return greedy(new_ind)
+            return i
+        return greedy(0)
