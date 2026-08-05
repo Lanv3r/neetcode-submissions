3fe64125176415d9110b8cb5d7a5d16f98e14822
Anver Ismagilov <64646822+Lanv3r@users.noreply.github.com>
2026-08-05 18:15:36 -0400

    Add: search-for-word-ii - submission-2

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-2.py
@@ -0,0 +1,57 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = False
+        self.index = -1
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word, index):
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.word = True 
+        cur.index = index
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        trie = Trie()
+        res = []
+
+        for i in range(len(words)):
+            trie.insert(words[i], i) 
+
+        def dfs(r, c, visited, root):
+            if (
+                r in range(ROWS) and 
+                c in range(COLS) and 
+                (r, c) not in visited 
+                and board[r][c] in root.children
+            ):
+                visited.add((r, c))
+                if r == 2 and c == 2:
+                    print(visited)
+                cur = root.children[board[r][c]]
+                if cur.word and cur.index >= 0:
+                    res.append(words[cur.index])
+                    cur.index = -1
+                dfs(r-1, c, visited, cur)
+                dfs(r+1, c, visited, cur)
+                dfs(r, c+1, visited, cur)
+                dfs(r, c-1, visited, cur) 
+                visited.remove((r, c))
+
+        ROWS, COLS = len(board), len(board[0])
+        cur = trie.root
+        for r in range(ROWS):
+            for c in range(COLS):
+                dfs(r, c, set(), cur)
+
+        return res
+                
+
+
